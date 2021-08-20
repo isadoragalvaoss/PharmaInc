6b23e5235bec58fc944acdedcad18a2c1fccaad7
Isadora Soares <isadoragalvaoss@gmail.com>
2021-08-20 18:47:57 -0300

    Adding axios

--- src/container/Front.js
+++ src/container/Front.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
+import { Nav } from "../components/Nav";
+import axios from "axios";
 
 function Front() {
+  const [item, setItem] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     axios.get("https://randomuser.me/api/?results=50").then((response) => {
       setItem(response.data.results);
@@ -8,7 +13,11 @@ function Front() {
     });
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <Nav />
+    </>
+  );
 }
 
 export default Front;
