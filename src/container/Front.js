b5d58b300988e298473d230ca4dea0884167689e
Isadora Soares <isadoragalvaoss@gmail.com>
2021-08-20 19:17:18 -0300

    Adding search input

--- src/container/Front.js
+++ src/container/Front.js
@@ -1,10 +1,14 @@
-import { useState, useEffect } from "react";
 import { Nav } from "../components/Nav";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import "../App.css";
 import { Table } from "../components/Table";
 
 function Front() {
   const [item, setItem] = useState([]);
+  const [searchTermNc, setSearchTermNc] = useState("");
+  const [searchTermName, setSearchTermName] = useState("");
+  const [searchTermGender, setSearchTermGender] = useState("");
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -14,8 +18,12 @@ function Front() {
     });
   }, []);
 
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
   return (
-    <>
+    <div>
       <Nav />
 
       <div className="container containerCard">
@@ -26,6 +34,47 @@ function Front() {
         <div className="row"></div>
 
         <>
+          <div className="row">
+            <div className="col-4">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search by name..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(event) => {
+                  setSearchTermName(event.target.value);
+                }}
+              ></input>
+            </div>
+            <div className="col-4">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search by nationality..."
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+                onChange={(event) => {
+                  setSearchTermNc(event.target.value);
+                }}
+              ></input>
+            </div>
+            <div className="col-4">
+              <select
+                class="form-select"
+                id="inputGroupSelect01"
+                onChange={(event) => {
+                  setSearchTermGender(event.target.value);
+                }}
+              >
+                <option value="neutro">Chosse..</option>
+
+                <option value="female">female</option>
+                <option value="male">male</option>
+              </select>
+            </div>
+          </div>
+
           <table className="table">
             <thead>
               <tr>
@@ -36,15 +85,48 @@ function Front() {
               </tr>
             </thead>
             <tbody>
-              {item.map((user) => (
-                <Table user={user} />
-              ))}
+              {item
+                .filter((user) => {
+                  if (searchTermName === "") {
+                    return user;
+                  } else if (
+                    user.name.first
+                      .toLowerCase()
+                      .includes(searchTermName.toLowerCase())
+                  ) {
+                    return user;
+                  }
+                })
+                .filter((user) => {
+                  if (searchTermNc === "") {
+                    return user;
+                  } else if (
+                    user.nat.toLowerCase().includes(searchTermNc.toLowerCase())
+                  ) {
+                    return user;
+                  }
+                })
+                .filter((user) => {
+                  if (searchTermGender == "neutro") {
+                    console.log("op1");
+                    return user;
+                  } else if (user.gender.includes(searchTermGender)) {
+                    let s = user.gender;
+                    s = s.normalize("NFKD");
+                    console.log(s);
+                    return user;
+                  }
+                })
+
+                .map((user) => (
+                  <Table user={user} />
+                ))}
             </tbody>
           </table>
           <button className="btn btn-dark mt-2">Loading More</button>
         </>
       </div>
-    </>
+    </div>
   );
 }
 
