0bf1edbbd5fc445eba1a5d1d05a663d2dec94036
Isadora Soares <isadoragalvaoss@gmail.com>
2021-08-20 18:57:47 -0300

    Adding Table and Modal

--- src/container/Front.js
+++ src/container/Front.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { Nav } from "../components/Nav";
 import axios from "axios";
+import { Table } from "../components/Table";
 
 function Front() {
   const [item, setItem] = useState([]);
@@ -16,6 +17,33 @@ function Front() {
   return (
     <>
       <Nav />
+
+      <div className="container containerCard">
+        <h2 className="text-center text-uppercase pt-4  font-weight-normal">
+          {" "}
+        </h2>
+
+        <div className="row"></div>
+
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Gender</th>
+                <th scope="col">Birth</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {item.map((user) => (
+                <Table user={user} />
+              ))}
+            </tbody>
+          </table>
+          <button className="btn btn-dark mt-2">Loading More</button>
+        </>
+      </div>
     </>
   );
 }
